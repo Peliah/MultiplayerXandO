45fa2eb0ade5091cde4c7595711ba1d1ccdc2ac1
Peliah <pelepoupa@gmail.com>
2024-09-24 23:44:42 +0100

    just checking

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')("https://multiplayer-xand-o.vercel.app/");
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
